c15d8a54455b9553bbae7034f556fcaa1a8659dd
luongnguyen0407 <lolwukonglee@gmail.com>
2022-12-28 18:12:31 +0700

    #5 - update banner trailer movie

--- components/partials/Header.tsx
+++ components/partials/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Logo from "/public/Logo.png";
 import Link from "next/link";
@@ -18,11 +18,26 @@ const LIST_MENU = [
   },
 ];
 const Header: React.FC = () => {
+  const [isTop, setIsTop] = useState(true);
   const router = useRouter();
   const pathName = router.pathname;
-
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsTop(window.scrollY === 0);
+    };
+    document.addEventListener("scroll", handleScroll);
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <div className="p-2 bg-primary">
+    <div
+      className={`fixed z-50 w-full p-2 transition duration-500 ${
+        !isTop
+          ? "bg-primary"
+          : "bg-gradient-to-b from-black/80 via-black/60 to-transparent"
+      } `}
+    >
       <div className="container flex items-center gap-x-10">
         <Image
           className="w-10 h-10 select-none"
